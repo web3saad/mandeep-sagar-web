07b4873571a1822c2b75e18ef5e32d784451624a
Mahammad Sayad <mahammadsalim817@gmail.com>
2026-06-16 11:47:49 +0530

    Add Netlify redirects

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
-import "./lib/error-capture";
+// import "./lib/error-capture";
 
-import { consumeLastCapturedError } from "./lib/error-capture";
+// import { consumeLastCapturedError } from "./lib/error-capture";
 import { renderErrorPage } from "./lib/error-page";
 
 type ServerEntry = {
@@ -30,7 +30,7 @@ async function normalizeCatastrophicSsrResponse(response: Response): Promise<Res
     return response;
   }
 
-  console.error(consumeLastCapturedError() ?? new Error(`h3 swallowed SSR error: ${body}`));
+console.error(new Error(`h3 swallowed SSR error: ${body}`));
   return new Response(renderErrorPage(), {
     status: 500,
     headers: { "content-type": "text/html; charset=utf-8" },
